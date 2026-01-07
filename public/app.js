cf6d82fd97dacdd1242fefb2d12d402b048ced52
yeonju73 <yeon703@naver.com>
2026-01-07 16:04:14 +0900

    feat: add current date

--- public/app.js
+++ public/app.js
@@ -161,3 +161,13 @@ scrollBtn.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+const data = document.getElementById("chatDate");
+const today = new Date();
+const formattedDate = today.toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+});
+
+data.textContent = formattedDate;
