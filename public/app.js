c0c3e750e6404bc90f9e5326c28fee875ed4018a
yeonju73 <yeon703@naver.com>
2026-01-07 12:11:37 +0900

    feat: add enter event handler

--- public/app.js
+++ public/app.js
@@ -4,27 +4,27 @@ import { chatbotApi } from './api/api.js';
 const inputTextArea = document.getElementById('send-message')
 const button = document.querySelector('button')
 
-button.addEventListener('click', async (event) => {
-    const inputText = inputTextArea.value.trim();
-    console.log("inputText:", inputText);
-
-    // 입력값이 공백일 경우 버튼 클릭 무시
-    if (!inputText) return;
-
-    // 사용자 말풍선 생성
-    addUserMessage(inputText);
-
-    // 입력창 초기화
-    inputTextArea.value = "";
-    const chatbotResult = await callChatbotApi(inputText);
-
-    // 챗봇 말풍선 생성
-    if (chatbotResult === null) {
-        addBotMessage("현재 챗봇 응답이 불가능합니다.");
-        return;
+const sendMessage = async () => {
+        const inputText = inputTextArea.value.trim();
+        console.log("inputText:", inputText);
+    
+        // 입력값이 공백일 경우 버튼 클릭 무시
+        if (!inputText) return;
+    
+        // 사용자 말풍선 생성
+        addUserMessage(inputText);
+    
+        // 입력창 초기화
+        inputTextArea.value = "";
+        const chatbotResult = await callChatbotApi(inputText);
+    
+        // 챗봇 말풍선 생성
+        if (chatbotResult === null) {
+            addBotMessage("현재 챗봇 응답이 불가능합니다.");
+            return;
+        }
+        addBotMessage(chatbotResult);
     }
-    addBotMessage(chatbotResult);
-});
 
 const callChatbotApi = async (inputText) => {
     const payload = {
@@ -70,3 +70,16 @@ const addBotMessage = (botText) => {
 
     messageDiv.scrollIntoView({ behavior: "smooth" });
 }
+
+// enter키 이벤트 핸들러
+inputTextArea.addEventListener('keydown', (event) => {
+    // enter와 shift키를 같이 눌렀을 때는 줄바꿈으로 생각
+    // enter만 눌렀을 때
+    if (event.key === "Enter" && !event.shiftKey) {
+        event.preventDefault(); // enter키 기본 동작인 줄바꿈 방지
+        sendMessage();
+    }
+})
+
+// button 이벤트 핸들러
+button.addEventListener('click', sendMessage);
